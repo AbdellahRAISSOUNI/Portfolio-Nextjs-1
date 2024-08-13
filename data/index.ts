8fcd4f3fc2939824550258f5f6b02691fc8aa330
AbdellahRAISSOUNI <abdellahraissouni@gmail.com>
2024-08-13 22:36:44 +0100

    done

---
+++ data/index.ts
@@ -0,0 +1,220 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Testimonials", link: "#testimonials" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "Client collaboration and open communication are my top priorities. ",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I’m available for communication across multiple time zones.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Developer with a Deep Passion for Tech Innovations.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "I am Abdellah Raissouni, a dedicated software engineering student specializing in developing cutting-edge websites using the latest technologies. My focus is on creating seamless, user-friendly web experiences while continuously advancing my skills in this dynamic field.",
+      description: "Who am I?",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "Ensate-Hub, an online Education Platform",
+      des: "Allowing teachers to connect with their students, by posting courses, assignments, tests and allowing students to interact and learn online efficiently,",
+      img: "/ensatehub.png",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+      link: "https://github.com/AbdellahRAISSOUNI/Ensate-Hub",
+    },
+    {
+      id: 2,
+      title: "Coming Soon",
+      des: "still cooking it",
+      img: "/p2.svg",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+      link: "",
+    },
+    {
+      id: 3,
+      title: "Coming Soon",
+      des: "still cooking it",
+      img: "/p3.svg",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+      link: "",
+    },
+    {
+      id: 4,
+      title: "Cpming Soon",
+      des: "still cooking it",
+      img: "/p4.svg",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+      link: "/ui.apple.com",
+    },
+  ];
+  
+  export const testimonials = [
+    {
+      quote:
+        "Collaborating with Abdellah was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Client 1",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Teaming up with Abdellah was a fantastic experience. His professionalism, reliability, and dedication to delivering top-notch results were evident throughout our collaboration. Abdellah’s passion for development shines through in every detail. If you’re looking to take your website and brand to the next level, Abdellah is the go-to expert.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Abdellah's collaboration was a highlight, marked by his impressive expertise and unwavering commitment. His enthusiasm for development shines through in every aspect of his work, demonstrating a genuine passion for excellence. For anyone looking to advance their website and brand, Abdellah stands out as the expert to rely on.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "With a remarkable blend of skill and dedication, Abdellah significantly contributed to the project. His deep passion for development was clear in the meticulous attention to detail he applied. For anyone aiming to enhance their website and brand, Abdellah proves to be an invaluable asset.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Abdellah made a profound impact on the project with his exceptional skills and unwavering dedication. The meticulous attention he devoted to every detail highlighted his passion for development. For those looking to elevate their website and brand, Abdellah is undeniably the expert to engage.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+  ];
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "cloudinary",
+      img: "/cloud.svg",
+      nameImg: "/cloudName.svg",
+    },
+    {
+      id: 2,
+      name: "appwrite",
+      img: "/app.svg",
+      nameImg: "/appName.svg",
+    },
+    {
+      id: 3,
+      name: "HOSTINGER",
+      img: "/host.svg",
+      nameImg: "/hostName.svg",
+    },
+    {
+      id: 4,
+      name: "stream",
+      img: "/s.svg",
+      nameImg: "/streamName.svg",
+    },
+    {
+      id: 5,
+      name: "docker.",
+      img: "/dock.svg",
+      nameImg: "/dockerName.svg",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Intern - NTTDATA",
+      desc: "Assisted in the development of a web-based platform using React.js, enhancing emlpoyees interactivity.",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+    {
+      id: 2,
+      title: "Mobile App Dev",
+      desc: "Designed and developed few mobile apps for Android platforms using Java and Kotlin .",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp2.svg",
+    },
+    {
+      id: 3,
+      title: "Freelance App Dev Project",
+      desc: "Led the dev of a few apps for clients, from initial concept to deployment on app stores.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp3.svg",
+    },
+    {
+      id: 4,
+      title: "Engineering Student",
+      desc: "A 4th yearEngineering student at the National School of Applied Sciences in Morocco",
+      className: "md:col-span-2",
+      thumbnail: "/exp4.svg",
+    },
+  ];
+  
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    url: "https://github.com/AbdellahRAISSOUNI",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    url: "https://twitter.com/",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    url: "https://ma.linkedin.com/in/abdellah-raissouni-1419432a8/en",
+  },
+];
